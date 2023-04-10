ffff75fa990fd90c8879576bd2d1dbf003519744
sidbird <123426974+sidbird@users.noreply.github.com>
2023-04-10 18:41:23 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 100);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet is initialized!');
+}
+
+function draw(){
+    background('#494f5f');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0){
+        console.log(results);
+    }
+}
+
+
+
+
