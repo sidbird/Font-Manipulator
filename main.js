0aa16e1937bedda57c67c30b9ab1f9c9ba8475b0
sidbird <123426974+sidbird@users.noreply.github.com>
2023-04-15 11:51:18 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,46 @@
-function setup(){
-    video = createCapture(VIDEO);
-    video.size(550, 500);
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 
-    canvas = createCanvas(550, 550);
-    canvas.position(560, 100);
+  function setup() {
+  video = createCapture(VIDEO);
+  video.size(550, 500);
 
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
+  canvas = createCanvas(550, 550);
+  canvas.position(560,150);
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
 
-function modelLoaded(){
-    console.log('PoseNet is initialized!');
+function modelLoaded() {
+  console.log('PoseNet Is Initialized!');
 }
 
-function draw(){
-    background('#494f5f');
-}
 
 function gotPoses(results)
 {
-    if(results.length > 0){
-        console.log(results);
-    }
+  if(results.length > 0)
+  {
+    console.log(results);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+  }
 }
 
+function draw() {
+background('#969A97');
 
-
-
+document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference +"px";
+  fill('#F80023');
+  textSize(difference)
+  text('Siddharth', 50, 400);
+  //fill('rgb(0,255,0)');
+//rect(20, 20, 60, 60);
+}
